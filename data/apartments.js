61f3979198e963db6318043ac4f36d5145d0d1a4
pocucapedja-debug <pocucapedja@gmail.com>
2026-09-14 22:38:36 +0200

    Add D6 apartment data

--- data/apartments.js
+++ data/apartments.js
@@ -1,3 +1,19 @@
 export const apartments = {
-  f7: { id:'f7', name:'Apartman F7', wifiName:'A1_SRB_B9F7D3', wifiPassword:'4KFhG7Tf', hostPhoneWa:'381642082838', reviewUrl:'https://maps.app.goo.gl/d55AtSNFWRUn9yaBA?g_st=ic', bookingUrl:'https://divcicasa.vercel.app/', instagramUrl:'https://www.instagram.com/divcicasa/' }
+  f7: {
+    id: 'f7',
+    name: 'Apartman F7',
+    wifiName: 'A1_SRB_B9F7D3',
+    wifiPassword: '4KFhG7Tf',
+    hostPhoneWa: '381666660015',
+    reviewUrl: 'https://www.google.com/maps/place/DivciCasa+apartments+Div%C4%8Dibare/'
+  },
+
+  d6: {
+    id: 'd6',
+    name: 'Apartman D6',
+    wifiName: 'M&D 2.4G / M&D 5G',
+    wifiPassword: 'EnjoyYourStay',
+    hostPhoneWa: '381666660015',
+    mapsUrl: 'https://maps.app.goo.gl/juLXoDXFnbHZPidG9'
+  }
 }
